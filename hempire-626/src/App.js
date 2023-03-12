0ab1a84e882d500055cb7b52c111144870139611
John Ramos <jramos5883@gmail.com>
2023-03-12 15:28:47 -0700

    Working on routing, nav bar, and home page.

--- hempire-626/src/App.js
+++ hempire-626/src/App.js
@@ -1,11 +1,33 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar!</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop!</h1>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
--- hempire-626/src/routes/home/home.component.jsx
+++ hempire-626/src/routes/home/home.component.jsx
@@ -1,5 +1,4 @@
 
-
 const Home = () => {
   return <div>Hello World!</div>;
 };
