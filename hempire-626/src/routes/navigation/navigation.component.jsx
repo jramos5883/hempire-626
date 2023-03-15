1301deefae76971fd5638625386cb74db989fda6
John Ramos <jramos5883@gmail.com>
2023-03-15 11:08:03 -0700

    Working on shopping cart and cart context.

--- hempire-626/src/routes/navigation/navigation.component.jsx
+++ hempire-626/src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,20 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as HempLogo } from "../../assets/HempireLogo.svg";
-import { UserContext } from "../../contexts/user.context";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+
+import { ReactComponent as HempLogo } from "../../assets/HempireLogo.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -49,7 +54,9 @@ const Navigation = () => {
         <Link className="nav-link" to="/shop">
           CART
         </Link>
+        <CartIcon />
       </div>
+      {isCartOpen && <CartDropdown />}
       <Outlet />
     </Fragment>
   );
